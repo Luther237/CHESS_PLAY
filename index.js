81fda2cf8d799e9972b67344522aad49091d4174
Luther237 <cheuwaluther@gmail.com>
2025-05-02 15:48:22 +0100

    Test d'erreur

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const containerPlateau = document.getElementById('container-plateau');
-    const tableauEchecs = document.createElement('table');
-    tableauEchecs.id = 'plateau-echecs';
-  
-    // Boucle pour créer les 8 lignes
-    for (let ligne = 8; ligne >= 1; ligne--) {
-      const ligneTable = document.createElement('tr');
-  
-      // Boucle pour créer les 8 colonnes (cases) dans chaque ligne
-      for (let colonneChar = 'a'; colonneChar <= 'h'; colonneChar = String.fromCharCode(colonneChar.charCodeAt(0) + 1)) {
-        const caseTable = document.createElement('td');
-        const idCase = colonneChar + ligne;
-        caseTable.id = idCase;
-        caseTable.classList.add('case');
-  
-        // Déterminer la couleur de la case
-        const estNoir = (ligne % 2 === 0 && colonneChar.charCodeAt(0) % 2 === 0) || (ligne % 2 !== 0 && colonneChar.charCodeAt(0) % 2 !== 0);
-        if (estNoir) {
-          caseTable.classList.add('noire');
-        } else {
-          caseTable.classList.add('blanche');
-        }
-  
-        ligneTable.appendChild(caseTable);
-      }
-  
-      tableauEchecs.appendChild(ligneTable);
-    }
-  
-    containerPlateau.appendChild(tableauEchecs);
-  });
+  function cellule(x, y, i, c){
+    this.x_position = x,
+    this.y_position = y,
+    this.background = i,
+    this.color = c;
+  }
+  let plateau = new Array(3);
+  for(let i=0; i<plateau.length; i++){
+    tab[i] = new Array(3)
+  }
+
+  for(let i=0; i<plateau.length; i++){
+    for(let j=0; j<plateau.length; j++){
+      id = i + "" + j;
+      plateau[i][j] = document.getElementById(id);
+    } 
+  }
+  plateau[0][0].innerHtml = "B";
+  console.log(id);
+
